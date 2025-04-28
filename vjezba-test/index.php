c3a233a3d8505586ce08a52fd80510c5847855cf
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-04-28 20:47:11 +0200

    main - vjezba-test

---
+++ vjezba-test/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vjezba - test</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Vježba - test</h1>
+  <hr>
+
+  <main class="main">
+    <form action="">
+      <label>
+        Upišite riječ: <br>
+        <input type="text" name="rijec">
+      </label>
+      <br><br>
+      <input type="submit" value="pošalji">
+    </form>
+    
+    <table border="1">
+      <tr>
+        <th>Riječi</th>
+        <th>Broj slova</th>
+        <th>Broj samoglasnika</th>
+        <th>Broj suglasnika</th>
+      </tr>
+
+      <tr>
+        <td>papa</td>
+        <td>4</td>
+        <td>2</td>
+        <td>2</td>
+      </tr>
+
+      <tr>
+        <td>crkva</td>
+        <td>5</td>
+        <td>1</td>
+        <td>4</td>
+      </tr>
+    </table>
+  </main>
+</body>
+</html>
