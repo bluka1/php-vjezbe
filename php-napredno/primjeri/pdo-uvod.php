f61f9c6ea7879fe5c68bca8a9463f645e334dbbd
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-06-17 21:00:23 +0200

    main - napredni-php - PDO - primjer - uvod

---
+++ php-napredno/primjeri/pdo-uvod.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbhost = 'localhost';
+$korisnik = 'root';
+$lozinka = 'Lozinka123.';
+$baza = 'zaposlenici';
+
+try {
+  $pdo = new PDO("mysql:host={$dbhost};dbname={$baza}", $korisnik, $lozinka);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+  $stmt = $pdo->prepare("SELECT * FROM zaposlenici");
+
+  $stmt->execute();
+
+  // dohvaćanje prvog zapisa
+  $prviZapis = $stmt->fetch();
+  foreach($prviZapis as $key => $value) {
+    echo $key .  ' : ' . $value;
+    echo '<br>';
+  }
+
+  echo '<hr>';
+
+  // dohvaćanje ostalih zapisa
+  while($row = $stmt->fetch()) {
+    echo $row['ime'] . ' ' . $row['prezime'] . '<br>';
+  }
+
+} catch(PDOException $e) {
+  echo 'Error: ' . $e->getMessage();
+}
